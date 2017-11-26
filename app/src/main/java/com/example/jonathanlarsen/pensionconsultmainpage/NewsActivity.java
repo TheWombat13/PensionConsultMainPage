7b0b4a0f8242946e538a9be588b8b4b12044a700
Mads <mads@stege.eu>
2017-11-26 18:58:44 +0100

    Created and connected Firebase backend to app. Implemented Firebase backend. Newsfeed still needs further work to function properly.

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/NewsActivity.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/NewsActivity.java
@@ -6,8 +6,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private ListView lv;
+
+    public List<Example> examples = new ArrayList<>();
+
+    class Example {
+        String titleString;
+        String urlString;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +37,8 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lv = (ListView) findViewById(R.id.listViewArticles);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +48,29 @@ public class NewsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DatabaseReference articleRef = database.getReference("example");
+
+        articleRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                List<Example> examples = new ArrayList<>();
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    examples.add(child.getValue(Example.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Failed to retrieve Database data!");
+            }
+        });
+        //    Needs further development!
+        //    ArrayAdapter<List> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, examples);
+        //    lv.setAdapter(arrayAdapter);
     }
+
 
 }
