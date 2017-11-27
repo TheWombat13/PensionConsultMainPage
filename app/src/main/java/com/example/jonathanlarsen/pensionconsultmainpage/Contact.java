9d59f31e324f63826084bfed730bb706121d90d3
JakobsenSWT <Jakobsen1608@hotmail.com>
2017-11-27 16:21:27 +0100

    Mail service

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/Contact.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/Contact.java
@@ -1,14 +1,22 @@
 package com.example.jonathanlarsen.pensionconsultmainpage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class Contact extends Activity {
+import javax.security.auth.Subject;
+
+public class Contact extends Activity implements View.OnClickListener {
+
+    private Button sendButton;
+
+    private String name, subject, comment;
 
     // Initiate spinner variables
     Spinner mySpinner;
@@ -19,11 +27,26 @@ public class Contact extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        sendButton = (Button) findViewById(R.id.button2);
+
         // Spinner
         mySpinner = (Spinner) findViewById(R.id.spinner);
         myAdapter = new ArrayAdapter<String>(Contact.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.contact_subjects));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+
+        startLayout();
+
+    }
+
+    public void onClick (View view) {
+        if (view == sendButton) {
+            sendMessage(view);
+        }
+    }
+
+    public void startLayout () {
+        sendButton.setOnClickListener(this);
     }
 
     // verify that String only contain letters
@@ -37,50 +60,52 @@ public class Contact extends Activity {
         return true;
     }
 
-    /*
-    // Check to see if the phone number is actually a phone number (Cannot contain +45 (atm)
-    public boolean isPhoneNumber(String name) {
-        char[] chars = name.toCharArray();
-        for (char c : chars) {
-            if(!Character.isDigit(c)) {
-                return false;
-            }
-        }
-        return true;
-    }*/
-
     public void sendMessage(View view) {
         // Get variables from editable text
         // Name
         EditText etName = (EditText) findViewById(R.id.etName);
-        String name = etName.getText().toString();
-        // phone number
-        EditText etPhone = (EditText) findViewById(R.id.etPhone);
-        String phone = etPhone.getText().toString();
-        // Mail
-        EditText etMail = (EditText) findViewById(R.id.etMail);
-        String mail = etMail.getText().toString();
+        name = etName.getText().toString();
+        // Subject
+        EditText etSubject = (EditText) findViewById(R.id.etMail);
+        subject = etSubject.getText().toString();
         // Spinner
         String test = mySpinner.getSelectedItem().toString();
         EditText etComment = (EditText) findViewById(R.id.etComment);
-        String comment = etComment.getText().toString();
+        comment = etComment.getText().toString();
 
-        // Validate name
-        if(isAlpha(name)) {
-            // Name is validated
-        }else {
-            // Name is not validated
-        }
+        validation();
 
-        /*
-        // Validate phone number
-        if(isPhoneNumber(phone)) {
-            // Phone is validated to contain numbers only
-        }else {
-            // Phone is not validated
-        }*/
+        /* Create the Intent */
+        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+        /* Fill it with Data */
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"jakobsen1608@hotmail.com"});
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject:" + subject);
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Comment: " + comment + "From: " + name);
+
+        /* Send it off to the Activity-Chooser */
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
 
 
     }
-}
 
+    public Boolean validation () {
+        // Validate name
+        if(isAlpha(name)) {
+            // Validate subject
+            if(subject != null) {
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        //ToDo Skal vel indeholde spinner logic?
+    }
+}
