b6c4863231b803ced9730ab13805a665dc44994a
Mads <Mads@stege.eu>
2017-11-30 15:23:04 +0100

    Reworks
    Small rework of Contact page - Still needs top toolbar.
    Reworked frontpage animation.

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/Contact.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/Contact.java
@@ -4,16 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
-import javax.security.auth.Subject;
 
 public class Contact extends Activity implements View.OnClickListener {
 
