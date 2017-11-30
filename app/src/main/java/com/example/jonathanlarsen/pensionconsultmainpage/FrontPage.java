bce3525f089ed9adfb529f31c5813568588d3d02
Mads <Mads@stege.eu>
2017-11-30 14:03:46 +0100

    New branch commit

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/FrontPage.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/FrontPage.java
@@ -3,18 +3,16 @@ package com.example.jonathanlarsen.pensionconsultmainpage;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.text.Html;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -38,7 +36,6 @@ public class FrontPage extends AppCompatActivity
 
         frontpageimg = (ImageView)findViewById(R.id.imageView);
         frontpageimg.setImageDrawable(getResources().getDrawable( R.drawable.frontpageanim));
-
 
         text = (TextView)findViewById(R.id.infotext);
         text.setText(Html.fromHtml(getString(R.string.Introtext)));
